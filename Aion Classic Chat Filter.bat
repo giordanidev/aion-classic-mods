ac4433f15f70ba00cc70d75227a9ee092cc082c4
LoadRNG <konkersgg@gmail.com>
2021-07-08 19:00:52 -0300

    Update Aion Classic Chat Filter.bat
    It will now set the filter file to read only so that the launcher doesn't replace it when there is a update (NCsoft never keeps track of all bot spam words anyways lol).

--- Aion Classic Chat Filter.bat
+++ Aion Classic Chat Filter.bat
@@ -9,37 +9,57 @@ FOR /F "tokens=2*" %%A IN ('REG QUERY "%KEY_NAME%" /v %VALUE_NAME% 2^>nul') DO (
 	set AIONDIR=%%B
 )
 
-ECHO Aion directory found: %AIONDIR%
+ECHO [%date% %time:~0,-3%] Aion directory found: %AIONDIR%
 
-xcopy /Y "%~dp0L10N\enu\Data\Strings\aionfilterline.dat" "%AIONDIR%\L10N\enu\Data\Strings\"
-if errorlevel 1 goto errorSD
-echo "-------------- Aion Chat Filter Updated --------------"
-endlocal
-pause
-exit
+set filterFile="%AIONDIR%L10N\enu\Data\Strings\aionfilterline.dat"
+
+if exist %filterFile% (
+	echo [%date% %time:~0,-3%] aionfilterline.dat file located. Path: 
+	echo [%date% %time:~0,-3%] %filterFile%
+	echo [%date% %time:~0,-3%] -------------------------------------------------------------
+	
+	powershell attrib -r %filterFile%
+	xcopy /Y "%~dp0L10N\enu\Data\Strings\aionfilterline.dat" "%AIONDIR%\L10N\enu\Data\Strings\"
+	if errorlevel 1 goto errorSD
+	powershell attrib +r %filterFile%
+	
+	echo [%date% %time:~0,-3%] --------------- Aion Chat Filter Updated and set to read only.
+	endlocal
+	pause
+	exit
+) else (
+	xcopy /Y "%~dp0L10N\enu\Data\Strings\aionfilterline.dat" "%AIONDIR%\L10N\enu\Data\Strings\"
+	if errorlevel 1 goto errorSD
+	powershell attrib +r %filterFile%
+
+	echo [%date% %time:~0,-3%] --------------- Aion Chat Filter Updated and set to read only.
+	endlocal
+	pause
+	exit
+)
 
 :errorSD
-echo "-------------- ERROR 'CF1': Filter file aionfilterline.dat is being used. Close the client or restart your computer and try again. --------------"
+echo [%date% %time:~0,-3%] --------------- ERROR 'CF1': Filter file aionfilterline.dat is being used or it is set to read only. Close the client or restart your computer and try again.
 endlocal
 pause
 exit
 
 :check_Permissions
-echo [%date% %time:~0,-3%] ----------------------------------------------------
+echo [%date% %time:~0,-3%] -------------------------------------------------------------
 echo [%date% %time:~0,-3%] Administrative permissions required. Detecting permissions...
-echo [%date% %time:~0,-3%] ----------------------------------------------------
+echo [%date% %time:~0,-3%] -------------------------------------------------------------
 
 net session >nul 2>&1
 if %errorLevel% == 0 (
 	echo [%date% %time:~0,-3%] Success: Administrative permissions confirmed.
-	echo [%date% %time:~0,-3%] ----------------------------------------------------
+	echo [%date% %time:~0,-3%] -------------------------------------------------------------
 ) else (
 	echo [%date% %time:~0,-3%] Failure: Current permissions inadequate.
-	echo [%date% %time:~0,-3%] ----------------------------------------------------
+	echo [%date% %time:~0,-3%] -------------------------------------------------------------
 	echo [%date% %time:~0,-3%] Run scritp as administrator: Right Click > Run as Admin
-	echo [%date% %time:~0,-3%] ----------------------------------------------------
+	echo [%date% %time:~0,-3%] -------------------------------------------------------------
 	echo [%date% %time:~0,-3%] Exiting script.
-	echo [%date% %time:~0,-3%] ----------------------------------------------------
+	echo [%date% %time:~0,-3%] -------------------------------------------------------------
 	pause
 	exit
 )
