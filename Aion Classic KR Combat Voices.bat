ff036c1ca70cc67c695397e28684ddb6484bc9c4
LoadRNG <konkersgg@gmail.com>
2021-07-08 13:04:41 -0300

    Create Aion Classic KR Combat Voices.bat

---
+++ Aion Classic KR Combat Voices.bat
@@ -0,0 +1,54 @@
+@echo off
+
+CALL :check_Permissions
+
+setlocal ENABLEEXTENSIONS
+set KEY_NAME=HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\NCWest\AION_CLASSIC
+set VALUE_NAME=BaseDir
+FOR /F "tokens=2*" %%A IN ('REG QUERY "%KEY_NAME%" /v %VALUE_NAME% 2^>nul') DO (
+	set AIONDIR=%%B
+)
+
+ECHO Aion directory found: %AIONDIR%
+
+if exist "%AIONDIR%\L10N\enu\Sounds\voice" (
+	ren "%AIONDIR%\L10N\enu\Sounds\voice" voiceNA
+	if errorlevel 1 goto errorSD
+	acho "-------------- NA COMBAT voices REMOVED. The client will now load the original KR COMBAT voices. --------------"
+	endlocal
+	pause
+	exit
+) else (
+	ren "%AIONDIR%\L10N\enu\Sounds\voiceNA" voice
+	if errorlevel 1 goto errorSD
+	eacho "-------------- NA COMBAT voices RESTORED. The client will now load the NA COMBAT voices. --------------"
+	endlocal
+	pause
+	exit
+)
+
+:errorSD
+echo "-------------- ERROR 'NAV1': Voices folder is being used. Close the client or restart your computer and try again. --------------"
+endlocal
+pause
+exit
+
+:check_Permissions
+echo [%date% %time:~0,-3%] ----------------------------------------------------
+echo [%date% %time:~0,-3%] Administrative permissions required. Detecting permissions...
+echo [%date% %time:~0,-3%] ----------------------------------------------------
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+	echo [%date% %time:~0,-3%] Success: Administrative permissions confirmed.
+	echo [%date% %time:~0,-3%] ----------------------------------------------------
+) else (
+	echo [%date% %time:~0,-3%] Failure: Current permissions inadequate.
+	echo [%date% %time:~0,-3%] ----------------------------------------------------
+	echo [%date% %time:~0,-3%] Run scritp as administrator: Right Click > Run as Admin
+	echo [%date% %time:~0,-3%] ----------------------------------------------------
+	echo [%date% %time:~0,-3%] Exiting script.
+	echo [%date% %time:~0,-3%] ----------------------------------------------------
+	pause
+	exit
+)
